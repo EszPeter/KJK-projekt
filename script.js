c7df52c74287581c050f4bed90adf747a95972b5
EszPeter <szatmaripeter2008@gmail.com>
2026-05-28 16:53:56 +0200

    megjelenitFejezet metódus első része

--- script.js
+++ script.js
@@ -16,10 +16,59 @@ fetch('konyv.json')
         console.error("Hiba történt a JSON betöltésekor:", error);
         document.getElementById('story-text').innerText = "Beolvasás sikertelen. Kérlek indítsd el a Live Servert!";
     });
-    
+
 function jatekInditasa() {
     aktualisFejezetId = 1;
     jatekos.eletero = 20;
     jatekos.hatizsak = ["Kard", "Élelem"];
     megjelenitFejezet(aktualisFejezetId);
+}
+
+
+function megjelenitFejezet(fejezetId) {
+    document.getElementById('hp-display').innerText = jatekos.eletero;
+    document.getElementById('inventory-display').innerText = jatekos.hatizsak.join(', ');
+
+    let fejezet = konyvAdatok.find(f => f.id === fejezetId);
+
+    if (!fejezet) {
+        document.getElementById('story-text').innerText = "A fejezet nem található a rendszerben.";
+        return;
+    }
+
+    document.getElementById('chapter-id').innerText = fejezetId + ". Pont";
+    document.getElementById('story-text').innerText = fejezet.szoveg;
+
+    if (fejezet.targy && fejezetId !== 10 && !jatekos.hatizsak.includes(fejezet.targy)) {
+        jatekos.hatizsak.push(fejezet.targy);
+        document.getElementById('inventory-display').innerText = jatekos.hatizsak.join(', ');
+    }
+
+
+    if (fejezet.sebzes) {
+        jatekos.eletero -= fejezet.sebzes;
+        if (jatekos.eletero < 0) jatekos.eletero = 0;
+        document.getElementById('hp-display').innerText = jatekos.eletero;
+    }
+    
+    if (fejezet.sebzes_pajzs_nelkul && !jatekos.hatizsak.includes("Pajzs")) {
+        jatekos.eletero -= fejezet.sebzes_pajzs_nelkul;
+        if (jatekos.eletero < 0) jatekos.eletero = 0;
+        document.getElementById('hp-display').innerText = jatekos.eletero;
+    }
+
+    let gombokContainer = document.getElementById('choices-container');
+    gombokContainer.innerHTML = '';
+
+    if (jatekos.eletero <= 0 || fejezet.halal) {
+        document.getElementById('story-text').innerText += "\n\nSajnálatos módon az életpontjaid elfogytak, vagy halálos csapdába estél. A kalandod itt véget ért.";
+        ujrainditasGomb();
+        return;
+    }
+
+
+    if (fejezet.gyozelem) {
+        ujrainditasGomb();
+        return;
+    }
 }
