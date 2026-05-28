d7a9c7233b90471e50ba5bf274a6288b86bf742a
EszPeter <szatmaripeter2008@gmail.com>
2026-05-28 16:37:50 +0200

    JSON fájl beolvasása

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ let jatekos = {
 
 let aktualisFejezetId = 1;
 let konyvAdatok = [];
+
+fetch('konyv.json')
+    .then(response => response.json())
+    .then(data => {
+        konyvAdatok = data;
+        jatekInditasa();
+    })
+    .catch(error => {
+        console.error("Hiba történt a JSON betöltésekor:", error);
+        document.getElementById('story-text').innerText = "Beolvasás sikertelen. Kérlek indítsd el a Live Servert!";
+    });
